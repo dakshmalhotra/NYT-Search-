ccab50e8ef12379e87d74e65cda7647f5830a895
rafactor <avila.rafael@gmail.com>
2018-11-05 21:28:43 -0500

    add Search Results

---
+++ assets/javascript/searchResults.js
@@ -0,0 +1,466 @@
+
+var data = {
+    "status": "OK",
+    "copyright": "Copyright (c) 2018 The New York Times Company. All Rights Reserved.",
+    "response": {
+      "docs": [
+        {
+          "web_url": "https://www.nytimes.com/1989/10/21/world/poland-s-premier-in-rome-seeks-aid.html",
+          "snippet": "LEAD: On his first trip abroad since taking office in August, Poland's Prime Minister made an urgent appeal today to Western governments to help his country deal with shortages of food and other supplies before winter.",
+          "print_page": "4",
+          "blog": {},
+          "source": "The New York Times",
+          "multimedia": [],
+          "headline": {
+            "main": "POLAND'S PREMIER, IN ROME, SEEKS AID",
+            "kicker": null,
+            "content_kicker": null,
+            "print_headline": null,
+            "name": null,
+            "seo": null,
+            "sub": null
+          },
+          "keywords": [
+            {
+              "name": "persons",
+              "value": "POPE",
+              "rank": 0,
+              "major": null
+            },
+            {
+              "name": "persons",
+              "value": "MAZOWIECKI, TADEUSZ",
+              "rank": 0,
+              "major": null
+            },
+            {
+              "name": "glocations",
+              "value": "POLAND",
+              "rank": 0,
+              "major": null
+            },
+            {
+              "name": "organizations",
+              "value": "ROMAN CATHOLIC CHURCH",
+              "rank": 0,
+              "major": null
+            },
+            {
+              "name": "subject",
+              "value": "ECONOMIC CONDITIONS AND TRENDS",
+              "rank": 0,
+              "major": null
+            },
+            {
+              "name": "subject",
+              "value": "FOREIGN AID",
+              "rank": 0,
+              "major": null
+            }
+          ],
+          "pub_date": "1989-10-21T00:00:00Z",
+          "document_type": "article",
+          "news_desk": "Foreign Desk",
+          "byline": {
+            "original": "By ALAN RIDING, Special to The New York Times",
+            "person": [
+              {
+                "firstname": "Alan",
+                "middlename": null,
+                "lastname": "RIDING",
+                "qualifier": null,
+                "title": null,
+                "role": "reported",
+                "organization": "",
+                "rank": 1
+              },
+              {
+                "firstname": null,
+                "middlename": null,
+                "lastname": null,
+                "qualifier": null,
+                "title": null,
+                "role": "reported",
+                "organization": "",
+                "rank": 2
+              }
+            ],
+            "organization": null
+          },
+          "type_of_material": "News",
+          "_id": "4fd1aa418eb7c8105d6c7bc7",
+          "word_count": 694,
+          "score": 1
+        },
+        {
+          "web_url": "https://query.nytimes.com/gst/abstract.html?res=9E0DE2DB153EE53ABC4F51DFB566838E649EDE",
+          "snippet": "",
+          "print_page": "R1",
+          "blog": {},
+          "source": "The New York Times",
+          "multimedia": [],
+          "headline": {
+            "main": "JERSEY PLANT STARTED; Standard Packaging to Have Factory in Clifton, N. J.",
+            "kicker": "1",
+            "content_kicker": null,
+            "print_headline": null,
+            "name": null,
+            "seo": null,
+            "sub": null
+          },
+          "keywords": [],
+          "pub_date": "1955-03-27T00:00:00Z",
+          "document_type": "article",
+          "type_of_material": "Article",
+          "_id": "4fc3b08145c1498b0d7ecf63",
+          "word_count": 127,
+          "score": 1
+        },
+        {
+          "web_url": "https://query.nytimes.com/gst/abstract.html?res=990DE1DF1F3BEE33A25756C0A9619C946297D6CF",
+          "snippet": "With the completion of the Commercial Pacific Company's cable last night, the dream of the late John W. Mackay of girdling the earth with his cable and telegraph systems was realized.",
+          "abstract": "Cable opened",
+          "print_page": "1",
+          "blog": {},
+          "source": "The New York Times",
+          "multimedia": [],
+          "headline": {
+            "main": "PRESIDENT OPENS NEW MANILA CABLE; Message Goes Around the World in 9 1/2 Minutes. Gov. Taft Sends Plea for Philippine Tariff Reduction Over the New Route -- Many Other Messages.",
+            "kicker": null,
+            "content_kicker": null,
+            "print_headline": null,
+            "name": null,
+            "seo": null,
+            "sub": null
+          },
+          "keywords": [
+            {
+              "name": "persons",
+              "value": "ROOSEVELT, THEODORE, PRESIDENT OF THE UNITED STATES",
+              "rank": 0,
+              "major": null
+            },
+            {
+              "name": "subject",
+              "value": "CABLES",
+              "rank": 0,
+              "major": null
+            }
+          ],
+          "pub_date": "1903-07-05T00:00:00Z",
+          "document_type": "article",
+          "type_of_material": "Front Page",
+          "_id": "4fc04afd45c1498b0d22d8e2",
+          "word_count": 1140,
+          "score": 1
+        },
+        {
+          "web_url": "https://query.nytimes.com/gst/abstract.html?res=9F00E4DA143AEE3ABC4951DFB566838A629EDE",
+          "snippet": "propeller blamed",
+          "abstract": "propeller blamed",
+          "print_page": "9",
+          "blog": {},
+          "source": "The New York Times",
+          "multimedia": [],
+          "headline": {
+            "main": "ITALIAN AIR FATALITY BLAMED ON PROPELLER; Loosened Blade Severed Tail of Colonel Maddalena's Plane, Plunging Three to Death.",
+            "kicker": null,
+            "content_kicker": null,
+            "print_headline": null,
+            "name": null,
+            "seo": null,
+            "sub": null
+          },
+          "keywords": [
+            {
+              "name": "persons",
+              "value": "MADDALENA , UMBERTO",
+              "rank": 0,
+              "major": null
+            }
+          ],
+          "pub_date": "1931-03-21T00:00:00Z",
+          "document_type": "article",
+          "type_of_material": "Article",
+          "_id": "4fc0b42c45c1498b0d41740b",
+          "word_count": 209,
+          "score": 1
+        },
+        {
+          "web_url": "https://slapshot.blogs.nytimes.com/2012/08/01/76000-pages-at-thousands-of-dollars-per-page/",
+          "snippet": "The N.H.L. gave the players' union thousands of pages of documents, but more have been requested in a high-stakes negotiation.",
+          "abstract": "The N.H.L. gave the players' union thousands of pages of documents, but more have been requested in a high-stakes negotiation.",
+          "blog": {},
+          "source": "The New York Times",
+          "multimedia": [],
+          "headline": {
+            "main": "In N.H.L. Negotiation, 76,000 Pages, at Thousands of Dollars Per Page",
+            "kicker": "Slap Shot",
+            "content_kicker": null,
+            "print_headline": null,
+            "name": null,
+            "seo": null,
+            "sub": null
+          },
+          "keywords": [
+            {
+              "name": "persons",
+              "value": "Bettman, Gary",
+              "rank": 1,
+              "major": null
+            },
+            {
+              "name": "persons",
+              "value": "Fehr, Donald",
+              "rank": 2,
+              "major": null
+            },
+            {
+              "name": "organizations",
+              "value": "National Hockey League",
+              "rank": 1,
+              "major": null
+            },
+            {
+              "name": "organizations",
+              "value": "National Hockey League Players' Assn",
+              "rank": 2,
+              "major": null
+            },
+            {
+              "name": "subject",
+              "value": "Hockey, Ice",
+              "rank": 1,
+              "major": null
+            }
+          ],
+          "pub_date": "2012-08-01T10:24:31Z",
+          "document_type": "blogpost",
+          "section_name": "Hockey",
+          "byline": {
+            "original": "By JEFF Z. KLEIN",
+            "person": [
+              {
+                "firstname": "Jeff",
+                "middlename": "Z.",
+                "lastname": "KLEIN",
+                "qualifier": null,
+                "title": null,
+                "role": "reported",
+                "organization": "",
+                "rank": 1
+              }
+            ],
+            "organization": null
+          },
+          "type_of_material": "Blog",
+          "_id": "50193d971c22dfde670b384a",
+          "word_count": 306,
+          "score": 1
+        },
+        {
+          "web_url": "https://www.nytimes.com/1989/10/21/business/key-rates-308589.html",
+          "snippet": "LEAD:               Previous   Year     In percent   Yesterday   Day   Ago              PRIME RATE   10.50   10.50   10.00        DISCOUNT RATE   7.00   7.00   6.50        FEDERAL FUNDS   8.68   8.68   8.29        3-MO. TREAS. BILLS   7.57   7.56 ...",
+          "print_page": "36",
+          "blog": {},
+          "source": "The New York Times",
+          "multimedia": [],
+          "headline": {
+            "main": "KEY RATES",
+            "kicker": null,
+            "content_kicker": null,
+            "print_headline": null,
+            "name": null,
+            "seo": null,
+            "sub": null
+          },
+          "keywords": [
+            {
+              "name": "subject",
+              "value": "KEY RATES (NYT COLUMN)",
+              "rank": 0,
+              "major": null
+            }
+          ],
+          "pub_date": "1989-10-21T00:00:00Z",
+          "document_type": "article",
+          "news_desk": "Financial Desk",
+          "type_of_material": "Statistics",
+          "_id": "4fd1aa418eb7c8105d6c7bcf",
+          "word_count": 110,
+          "score": 1
+        },
+        {
+          "web_url": "https://query.nytimes.com/gst/abstract.html?res=9804E6D7153AEE3ABC4052DFB166838A629EDE",
+          "snippet": "Leaves for Portsmouth, Me",
+          "abstract": "Leaves for Portsmouth, Me",
+          "print_page": "19",
+          "blog": {},
+          "source": "The New York Times",
+          "multimedia": [],
+          "headline": {
+            "main": "Birth Notice 1 -- No Title",
+            "kicker": null,
+            "content_kicker": null,
+            "print_headline": null,
+            "name": null,
+            "seo": null,
+            "sub": null
+          },
+          "keywords": [
+            {
+              "name": "persons",
+              "value": "MEADOWCROFT, WM HY",
+              "rank": 0,
+              "major": null
+            }
+          ],
+          "pub_date": "1931-07-18T00:00:00Z",
+          "document_type": "article",
+          "type_of_material": "Birth Notice",
+          "_id": "4fc0b42c45c1498b0d417411",
+          "word_count": 139,
+          "score": 1
+        },
+        {
+          "web_url": "https://www.nytimes.com/1964/11/09/mums-displayed-in-motif-of-japan.html",
+          "snippet": "chrysanthemum show, NY Botanical Garden; illus",
+          "abstract": "chrysanthemum show, NY Botanical Garden; illus",
+          "blog": {},
+          "source": "The New York Times",
+          "multimedia": [],
+          "headline": {
+            "main": "MUMS DISPLAYED IN MOTIF OF JAPAN",
+            "kicker": null,
+            "content_kicker": null,
+            "print_headline": null,
+            "name": null,
+            "seo": null,
+            "sub": null
+          },
+          "keywords": [
+            {
+              "name": "subject",
+              "value": "HORTICULTURE",
+              "rank": 1,
+              "major": null
+            }
+          ],
+          "pub_date": "1964-11-09T00:00:00Z",
+          "document_type": "article",
+          "type_of_material": "News",
+          "_id": "5360131038f0d87ca6edfc10",
+          "word_count": 198,
+          "score": 1
+        },
+        {
+          "web_url": "https://query.nytimes.com/gst/abstract.html?res=9907E1DD1539E433A25756C1A9649D946297D6CF",
+          "snippet": "Warner, J. De W., protest water fee tales",
+          "abstract": "Warner, J. De W., protest water fee tales",
+          "print_page": "16",
+          "blog": {},
+          "source": "The New York Times",
+          "multimedia": [],
+          "headline": {
+            "main": "John De Witt Warner's Compensation.",
+            "kicker": null,
+            "content_kicker": null,
+            "print_headline": null,
+            "name": null,
+            "seo": null,
+            "sub": null
+          },
+          "keywords": [
+            {
+              "name": "persons",
+              "value": "WARNER, J. DE W.",
+              "rank": 0,
+              "major": null
+            }
+          ],
+          "pub_date": "1903-12-15T00:00:00Z",
+          "document_type": "article",
+          "byline": {
+            "original": "JOHN DE WITT WARNER",
+            "person": [
+              {
+                "firstname": "John",
+                "middlename": null,
+                "lastname": "DE WITT",
+                "qualifier": null,
+                "title": null,
+                "role": "reported",
+                "organization": "",
+                "rank": 1
+              }
+            ],
+            "organization": null
+          },
+          "type_of_material": "Letter",
+          "_id": "4fc04afd45c1498b0d22d8e7",
+          "word_count": 212,
+          "score": 1
+        },
+        {
+          "web_url": "https://www.nytimes.com/1863/09/23/news/amusements-this-evening.html",
+          "snippet": "",
+          "print_page": "4",
+          "blog": {},
+          "source": "The New York Times",
+          "multimedia": [],
+          "headline": {
+            "main": "Amusements this Evening.",
+            "kicker": null,
+            "content_kicker": null,
+            "print_headline": null,
+            "name": null,
+            "seo": null,
+            "sub": null
+          },
+          "keywords": [],
+          "pub_date": "1863-09-23T00:03:58Z",
+          "document_type": "article",
+          "type_of_material": "Article",
+          "_id": "4fbfdf8145c1498b0d03fa63",
+          "word_count": 74,
+          "score": 1
+        }
+      ],
+      "meta": {
+        "hits": 15538569,
+        "offset": 0,
+        "time": 171
+      }
+    }
+  }
+  
+  console.log(data)
+  data.response.docs.forEach(element => {
+    var headline = element.headline.main;
+    var byline  = (element.byline) ? element.byline.original : '';
+    var snippet = element.snippet;
+    var pub_date = new Date(element.pub_date)
+    var temp = pub_date.toDateString();
+    console.log(temp)
+    var type_of_material = element.type_of_material;
+
+    var $wrapper = $('<div>');
+    var $headline = $('<h3>');
+    var $byline = $('<small>');
+    var $snippet = $('<p>');
+    var $pub_date = $('<span>');
+    var $type = $('<p>');
+
+    $headline.addClass('headline').text(headline)
+    $byline.addClass('byline').text(byline)
+    $snippet.addClass('snippet').text(snippet)
+    $pub_date.addClass('pub-date').text(pub_date)
+    $type.addClass('type').text(type_of_material)
+
+    $wrapper.append($headline, $byline, $snippet, $pub_date, $type)
+    $('#top-articles').append($wrapper)
+
+
+  });
+
+  // console.log(response)
