a81480342d9f29a02a1dcecad5090d73940660e1
ibsafi <ibraheemsafi@gmail.com>
2018-11-07 19:16:27 -0500

    Ibrahim commit

--- assets/javascript/searchResults.js
+++ assets/javascript/searchResults.js
@@ -5,12 +5,18 @@ function send_request_handler(){
         'api-key': "a80bbaccde51425ebd955684ea745fb0",
         'q': "",  //query  
         'sort': "newest",
-        'begin_date': "", //start date
-        'end_date': ""    //end
+ //       'begin_date': "", //start date
+ //       'end_date': ""    //end
     };
-    request_obj.q           = $("#query").val();
-    request_obj.begin_date  = $("#begin_date").val() + "0101";
-    request_obj.end_date    = $("#end_date").val() + "1231";
+    request_obj.q   = $("#query").val();
+    var begin_date  = $("#begin_date").val();
+    var end_date    = $("#end_date").val();
+    if(begin_date.length > 0){
+        request_obj.begin_date  = $("#begin_date").val() + "0101";
+    }
+    if(end_date.length > 0){
+        request_obj.end_date    = $("#end_date").val() + "1231";
+    }
 
     var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
     url += '?' + $.param(request_obj);
@@ -18,9 +24,4 @@ function send_request_handler(){
       url: url,
       method: 'GET',
     }).done(receive_request_handler).fail(function(err) {  throw err;    });
-}
-
-function receive_request_handler(response){
-    
-
 }
